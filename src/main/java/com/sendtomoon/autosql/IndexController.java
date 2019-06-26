85373fa4b24d2d8a6cfbfb9ea8b4f3b0523f1a06
galaxysha512 <galaxysha512@gmail.com>
2019-06-26 10:39:28 +0800

    zip controller

--- src/main/java/com/sendtomoon/autosql/IndexController.java
+++ src/main/java/com/sendtomoon/autosql/IndexController.java
@@ -1,10 +1,12 @@
 package com.sendtomoon.autosql;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -23,9 +25,10 @@ public class IndexController {
 	@ResponseBody
 	public String save(String datas, String startNo, String editor, String tableName, String tableComment)
 			throws Exception {
-		this.create(datas, startNo, editor, tableName, tableComment);
-		this.syn(startNo, editor, tableName);
-		this.grt(startNo, editor, tableName);
+		String create = this.create(datas, startNo, editor, tableName, tableComment);
+		String syn = this.syn(startNo, editor, tableName);
+		String grt = this.grt(startNo, editor, tableName);
+		this.zip(create, syn, grt);
 		return "successful";
 	}
 
@@ -40,7 +43,7 @@ public class IndexController {
 		return no;
 	}
 
-	private void grt(String startNo, String editor, String tableName) throws Exception {
+	private String grt(String startNo, String editor, String tableName) throws Exception {
 		File file = new File(this.getNumber(startNo, 2) + "_ommdata_ddl_grt_" + tableName + "_" + editor + ".sql");
 		FileOutputStream out = new FileOutputStream(file, false);
 		StringBuffer grtSB = new StringBuffer();
@@ -49,9 +52,10 @@ public class IndexController {
 		out.write(grtSB.toString().getBytes("utf-8"));
 		out.flush();
 		out.close();
+		return file.getPath();
 	}
 
-	private void create(String datas, String startNo, String editor, String tableName, String tableComment)
+	private String create(String datas, String startNo, String editor, String tableName, String tableComment)
 			throws Exception {
 		List<RowDataDTO> list = JSON.parseArray(datas, RowDataDTO.class);
 
@@ -81,9 +85,10 @@ public class IndexController {
 		out.write(createSB.toString().getBytes("utf-8"));
 		out.flush();
 		out.close();
+		return file.getPath();
 	}
 
-	private void syn(String startNo, String editor, String tableName) throws Exception {
+	private String syn(String startNo, String editor, String tableName) throws Exception {
 		File file = new File(this.getNumber(startNo, 1) + "_ommdata_ddl_syn_" + tableName + "_" + editor + ".sql");
 		FileOutputStream out = new FileOutputStream(file, false);
 		StringBuffer synSB = new StringBuffer();
@@ -91,6 +96,7 @@ public class IndexController {
 		out.write(synSB.toString().getBytes("utf-8"));
 		out.flush();
 		out.close();
+		return file.getPath();
 	}
 
 	private String isNull(boolean isnull) {
@@ -104,4 +110,66 @@ public class IndexController {
 		return "";
 	}
 
+	public boolean zip(String create, String syn, String grt) {
+		byte[] buffer = new byte[512];// 缓冲器
+		ZipEntry zipEntry = null;
+		int readLength = 0;// 每次读出来的长度
+
+		try {
+			ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("SQL.zip"));
+			File createFile = new File(create);
+			File synFile = new File(syn);
+			File grtFile = new File(grt);
+			zipEntry = new ZipEntry(create);
+			zipEntry.setSize(createFile.length());
+			zipEntry.setTime(createFile.lastModified());
+			zipOutputStream.putNextEntry(zipEntry);
+			InputStream inputStream = new BufferedInputStream(new FileInputStream(createFile));
+			while ((readLength = inputStream.read(buffer, 0, 512)) != -1) {
+				zipOutputStream.write(buffer, 0, readLength);
+			}
+			inputStream.close();
+			System.out.println("file compressed: " + createFile.getCanonicalPath());
+
+			zipEntry = new ZipEntry(syn);
+			zipEntry.setSize(synFile.length());
+			zipEntry.setTime(synFile.lastModified());
+			zipOutputStream.putNextEntry(zipEntry);
+			inputStream = new BufferedInputStream(new FileInputStream(synFile));
+			while ((readLength = inputStream.read(buffer, 0, 512)) != -1) {
+				zipOutputStream.write(buffer, 0, readLength);
+			}
+			inputStream.close();
+			System.out.println("file compressed: " + synFile.getCanonicalPath());
+
+			zipEntry = new ZipEntry(grt);
+			zipEntry.setSize(grtFile.length());
+			zipEntry.setTime(grtFile.lastModified());
+			zipOutputStream.putNextEntry(zipEntry);
+			inputStream = new BufferedInputStream(new FileInputStream(grtFile));
+			while ((readLength = inputStream.read(buffer, 0, 512)) != -1) {
+				zipOutputStream.write(buffer, 0, readLength);
+			}
+			inputStream.close();
+			System.out.println("file compressed: " + grtFile.getCanonicalPath());
+
+			zipOutputStream.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			System.out.println("zip fail!");
+
+			return false;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			System.out.println("zip fail!");
+
+			return false;
+		}
+
+		System.out.println("zip success!");
+
+		return true;
+	}
 }
